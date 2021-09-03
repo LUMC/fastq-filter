9aa1155c2e00cc507c3d30f0672adc13ab856d7a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-09-03 13:48:07 +0200

    Add FASTQ parser and mean quality filter

--- src/fastq_filter/__init__.py
+++ src/fastq_filter/__init__.py
@@ -17,6 +17,51 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import io
+import math
+import typing
+from typing import Generator
+
+import numpy as np
+
+
+class FastqRecord(typing.NamedTuple):
+    name: bytes
+    sequence: bytes
+    plus: bytes
+    phred_scores: bytes
+
+
+def file_to_fastq_records(filepath) -> Generator[FastqRecord, None, None]:
+    with open(filepath, "rb") as file_h:  # type: io.BufferedReader
+        while True:
+            name = file_h.readline()
+            if name == b"":
+                return
+            if not name.startswith(b"@"):
+                raise ValueError("Record header should start with '@'.")
+            try:
+                sequence = next(file_h)
+                plus = next(file_h)
+                qualities = next(file_h)
+            except StopIteration:
+                raise ValueError("Truncated fastq record at EOF")
+            if len(sequence) != len(qualities):
+                raise ValueError(f"Fastq record with sequence and qualities "
+                                 f"of unequal length at byte: "
+                                 f"{file_h.tell()}")
+            yield FastqRecord(name.rstrip(),
+                              sequence.rstrip(),
+                              plus.rstrip(),
+                              qualities.rstrip())
+
+
+def mean_quality_filter(record: FastqRecord, quality: float) -> bool:
+    phred_scores = np.frombuffer(record.phred_scores, dtype=np.uint8)
+    qualities = 10 ^ (phred_scores / -10)
+    average = np.average(qualities)
+    return -10 * math.log10(average) >= quality
+
 
 def main():
     pass
