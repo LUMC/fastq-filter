de7336505d6f0011c7e930a82e5503a4085cf3a6
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2021-09-08 17:38:08 +0200

    Better order to catch errors

--- src/fastq_filter/_optimize.pyx
+++ src/fastq_filter/_optimize.pyx
@@ -8,18 +8,22 @@ from libc.stdint cimport uint8_t, uint_fast32_t
 from libc.math cimport log10
 
 def qualmean(bytes qualities, double phred_offset = 33):
+    """
+    Returns the mean of the quality scores in an ASCII quality string as stored
+    in a FASTQ file. Returns a float value.
+    """
+    cdef double phred_constant = 10 ** -0.1
+    cdef double sum_probabilities = 0.0
+    cdef double average
+    cdef double average_phred
     cdef Py_buffer buffer_data
     cdef Py_buffer* buffer = &buffer_data
     # Cython makes sure error is handled when acquiring buffer fails.
     PyObject_GetBuffer(qualities, buffer, PyBUF_C_CONTIGUOUS)
     cdef uint8_t *scores = <uint8_t *>buffer.buf
-    if buffer.len == 0:
-        raise ValueError("Empty quality string")
-    cdef double phred_constant = 10 ** -0.1
-    cdef double sum_probabilities = 0.0
-    cdef double average
-    cdef double average_phred
     try:
+        if buffer.len == 0:
+            raise ValueError("Empty quality string")
         for i in range(buffer.len):
             sum_probabilities += phred_constant ** scores[i]
         average = sum_probabilities / buffer.len
