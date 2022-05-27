3337e462939c05364cb0afca685ba905bd06fe60
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-05-27 08:32:58 +0200

    Add code generation script

---
+++ score_to_error_rate.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    with open("src/fastq_filter/score_to_error_rate.h", "wt") as output:
+        output.write("// An array holding all error rates for each phred score.\n")
+        output.write("// This file was automatically generated.\n\n")
+        output.write("static const double SCORE_TO_ERROR_RATE[128] = {\n")
+        for i in range(128):
+            error_rate = 10 ** -(i/10)
+            output.write(f"    {str(error_rate).upper() + 'L,':24}  // {i}\n")
+        output.write("};\n")
